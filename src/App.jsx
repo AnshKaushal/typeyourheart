976086cc833ec613503d98ea1dc16aa752ee2360
AnshKaushal <ansh1043.be21@chitkarauniversity.edu.in>
2022-07-31 19:34:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <form action="#">
+        <textarea name="text" id="inputText" cols="30" rows="10" autoFocus></textarea>
+      </form>
+    </div>
+  );
+}
+
+export default App;
