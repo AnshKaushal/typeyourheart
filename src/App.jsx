f698926f8a229574f342a320f71085ecf488e394
AnshKaushal <ansh1043.be21@chitkarauniversity.edu.in>
2022-10-19 21:07:47 +0530

    changed website somewhat

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="heading">Type your heart out!</h1>
       <form action="#">
         <textarea name="text" id="inputText" cols="30" rows="10" autoFocus></textarea>
       </form>
-      <footer>
-        Developed By <a href="https://github.com/AnshKaushal"> Ansh Kaushal.</a>
-      </footer>
     </div>
   );
 }
